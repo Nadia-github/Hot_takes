ad4f5dc481c86db8880833d2c5aadadf406b226f
Nadia-github <nadia.lahssini@yahoo.fr>
2021-12-02 12:28:54 +0100

    modifs

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const express = require('express');
+const userRoute = require ('./routes/user')
+
+const saucesRoute = require ('./routes/sauces')
+
+const express = require ('express');
+
+const bodyParser = require ('body-parser');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
- });
+const mongoose = require('mongoose');
 
-module.exports = app;
+const sauces = require('./models/sauces');
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use('/api/auth', userRoute)
+
+  app.use(bodyParser.json())
+
+  app.use('/api/sauces', saucesRoute);
+
+  mongoose.connect('mongodb+srv://Nadia:123123123@cluster0.b94si.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+  module.exports = app;
+
