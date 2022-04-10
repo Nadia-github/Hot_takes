4acbb2b7f23343976ff5382b5cf73dc44cf7d52d
Nadia-github <nadia.lahssini@yahoo.fr>
2022-04-10 19:55:34 +0200

    Ajout commentaires explicatifs

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,13 +4,15 @@ require ("dotenv").config();
 
 module.exports = (req, res, next) => {
   try {
+    // on récupère les entêtes de la requête et on cherche le paramètre "autorization" parmi ces entêtes 
     const token = req.headers.authorization.split(' ')[1]; 
-    /* Ici on utilise le module JWT afin de vérifié le token pour savoir si c'est un vrai utilisateur 
-    Le token est crypté d'une certaine façon que l'on peut savoir si c'est un véritable token*/
+    // Ici on utilise le module JWT afin de vérifié le token pour savoir si c'est un vrai utilisateur 
     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-    // on met le userId dans le token pour savoir qui est la personne connectée
+    // on met le userId dans le token 
     const userId = decodedToken.userId;
+    // on passe la donnée d'une fonction à une autre, qu'elle reste dans la réponse
     res.locals.userId = userId;
+    // Si l'utilisateur a envoyé un userId dans sa requête, on vérifie si le userId est bien le sien
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
